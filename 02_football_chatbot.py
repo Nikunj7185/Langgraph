b4f26fa9ac2553eb7ed3024946a843aa5aefd288
Nikunj7185 <nkunj7185@gmail.com>
2025-04-11 09:58:27 +0530

    first commit

---
+++ 02_football_chatbot.py
@@ -0,0 +1,69 @@
+import getpass
+import os
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+load_dotenv()
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+    
+graph_builder = StateGraph(State)
+from langchain_groq import ChatGroq
+
+llm=ChatGroq(model="llama-3.1-8b-instant")
+
+def chatbot(state:State):
+    return {"messages":[llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+from IPython.display import Image, display
+
+try:
+    display(Image(graph.get_graph().draw_mermaid_png()))
+except Exception:
+    # This requires some extra dependencies and is optional
+    pass
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [
+            {"role": "system", "content": (
+                '''
+                You are a friendly and knowledgeable football assistant. 
+                You are here to help users with anything related to football including rules, playing positions, teams, player history, training tips,     
+                as well as football gear like shoes, studs, jerseys, and training equipment. 
+                If the user asks for recommendations (like best studs for beginners), suggest beginner-friendly options available in general markets.
+                '''
+                )
+            },
+            {"role": "user", "content": user_input}
+        ]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        # print("User: " + user_input)
+        stream_graph_updates(user_input)
+    except exception as e:
+        print(f"An error occurred: {e}")
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
